b8ad865044264562418312f7aab2a351f1dc5f69
Adm-Tariq <adm.tariq11@gmail.com>
2023-11-30 16:11:28 +0200

    Create Git Repository

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ function App() {
       <header className="App-header">
         <a href="/">amazon</a>
       </header>
-      <main>list product</main>
+      <main>list product ش</main>
     </div>
   );
 }
