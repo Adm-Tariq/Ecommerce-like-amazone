9c23b244a678131724794996206f5141f2db6f45
Adm-Tariq <adm.tariq11@gmail.com>
2023-11-30 19:00:15 +0200

    update

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+import data from "./data.js";
+
+const app = express();
+const port = 5000;
+
+app.get("/api/products", (req, res) => res.send(data.products));
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
